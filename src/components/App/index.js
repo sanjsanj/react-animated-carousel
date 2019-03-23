2cc86c9a01958b17d3688ff0fbde3e3a09f89291
Sanjay Purswani <sanjsanj@hotmail.com>
2019-03-23 18:48:34 +0000

    Horizontally align slides in track

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Slider from "react-slick";
+import styled from "styled-components";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -8,22 +9,30 @@ import Slide from "../Slide";
 
 const slides = ["tall", "short", "tall", "short", "tall"];
 
+const StyledSlider = styled(Slider)`
+  .slick-track {
+    align-items: center;
+    display: flex;
+  }
+`;
+
 class App extends React.Component {
   render() {
     const settings = {
       slidesToShow: 1.3,
       slidesToScroll: 1,
+      initialSlide: 0,
       infinite: false,
       dots: false,
     };
 
     return (
       <div>
-        <Slider {...settings}>
+        <StyledSlider {...settings}>
           {slides.map((slide, index) => (
             <Slide key={index} tall={slide === "tall"} />
           ))}
-        </Slider>
+        </StyledSlider>
       </div>
     );
   }
